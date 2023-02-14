a3776ef71287e29e85356e6b6806d19e77e34620
M1chae1M <michalmaru10@gmail.com>
2023-02-14 13:41:21 +0100

    with next.js

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,18 @@ class Pokemon{
    }
 }
 
-const fetchFunction = (numberOfPage, targ, changeState) => {
-    if (targ > 0) {
-      let allFetchesInTable = [];
-      for (let i = numberOfPage * targ + 1; i < numberOfPage * targ + targ + 1; i++) {
-        let url = "https://pokeapi.co/api/v2/pokemon/" + i;
+const fetchFunction=(numberOfPage, targ, changeState)=>{
+    if(targ>0){
+      let allFetchesInTable=[];
+      for(let i=numberOfPage*targ+1;i< numberOfPage*targ+targ+1;i++){
+        let url="https://pokeapi.co/api/v2/pokemon/"+i;
         fetch(url)
-          .then((res) => res.json())
-          .then((res) => {
+          .then((res)=>res.json())
+          .then((res)=>{
             allFetchesInTable.push(new Pokemon(res.id, res.name, res.types, res.weight));
             return allFetchesInTable;
           })
-          .then((allFetchesInTable) => changeState(allFetchesInTable));
+          .then((allFetchesInTable)=>changeState(allFetchesInTable));
       }
     } else {
       changeState([]);
