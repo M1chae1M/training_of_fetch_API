a3e8129d0bfda2fcf8bad89a6a0c5c0364d52e7d
M1chae1M <michalmaru10@gmail.com>
2023-02-14 14:17:02 +0100

    with next.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document(){
   return(
     <Html lang="pl">
-      <Head>
-        <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
-        <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
-      </Head>
+      <Head/>
       <body>
         <Main/>
         <NextScript/>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './component/App';
-// import Head from 'next/head';
 
 class Pokemon{
     constructor(ID, name, type, weight){
