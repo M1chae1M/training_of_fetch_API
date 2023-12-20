861cb08a1f8362223022add3a4a2105c0104de69
M1chae1M <michalmaru10@gmail.com>
2023-12-20 12:48:59 +0100

    little less code & console errors 5

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,6 @@ class Pokemon{
    }
 }
 
-const fetchFunction=(numberOfPage, targ, changeState)=>{
-    if(targ>0){
-        const allFetchesInTable=[];
-        for(let i=numberOfPage*targ+1;i<numberOfPage*targ+targ+1;i++){
-            const url=`https://pokeapi.co/api/v2/pokemon/${i}`;
-            fetch(url)
-            .then((res)=>res.json())
-            .then(({id,name,types,weight})=>allFetchesInTable.push(new Pokemon(id,name,types,weight)))
-            .then((res)=>changeState(allFetchesInTable))
-        }
-    }else{changeState([])}
-}
-
 export default class RenderAndApiURL extends React.Component{
     state={
         allFetchesInTableState:[],
@@ -30,18 +17,32 @@ export default class RenderAndApiURL extends React.Component{
         displayedPokemonsOnPage:100,
         testState:false,
     }
-    componentDidMount(e){
-        fetchFunction(this.state.numberOfPage,this.state.displayedPokemonsOnPage,(newState)=>this.setState({allFetchesInTableState:newState}));
+    fetchFunction=(targ)=>{
+        const {numberOfPage}=this.state
+        if(targ>0){
+            const allFetchesInTable=[];
+            for(let i=numberOfPage*targ+1;i<numberOfPage*targ+targ+1;i++){
+                const url=`https://pokeapi.co/api/v2/pokemon/${i}`;
+                fetch(url)
+                .then((res)=>res.json())
+                .then(({id,name,types,weight})=>allFetchesInTable.push(new Pokemon(id,name,types,weight)))
+                .then((res)=>this.setState({allFetchesInTableState:allFetchesInTable}))
+            }
+        }else{
+            this.setState({allFetchesInTableState:[]})
+        }
+    }
+    componentDidMount(){
+        this.fetchFunction(this.state.displayedPokemonsOnPage);
     }
     render(){
         const {displayedPokemonsOnPage,allFetchesInTableState,numberOfPage}=this.state
-        const changeFetchesState=(tab)=>this.setState({allFetchesInTableState:tab})
         let timeoutID;
         const debounce=(e)=>{
             const {value}=e.target
             if(timeoutID){clearTimeout(timeoutID)}
             timeoutID=setTimeout(()=>{
-                fetchFunction(numberOfPage,parseInt(value),changeFetchesState);
+                this.fetchFunction(parseInt(value));
                 this.setState({displayedPokemonsOnPage:parseInt(value)});
             },1000);
         }
